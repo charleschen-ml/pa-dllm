83b51ff664b2bb386c0a76d0e45b33bdba185044
Charles Chen <charleschen0305@gmail.com>
2025-08-26 16:26:04 -0700

    a

--- inference.py
+++ inference.py
@@ -341,7 +341,7 @@ def run_inference(model, tokenizer, device, prompt, model_args, max_new_tokens=3
     optimal_block_sizes = None
     min_step = float('inf')
     
-    for sweep_value in range(2, 8):
+    for sweep_value in range(2, 4):
         manual_settings = {
             0: 23, 
             1: 2, 
