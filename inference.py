f76be1099826a6fca48366703420e2991b2b2e32
Charles Chen <charleschen0305@gmail.com>
2025-09-02 16:54:30 -0700

    a

--- inference.py
+++ inference.py
@@ -514,6 +514,9 @@ def run_inference_batch(model, tokenizer, device, model_args, input_csv_path, ou
         prompt = tokenizer.apply_chat_template(messages, add_generation_prompt=True, tokenize=False)
         input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
 
+        # print decoded input
+        print(f"decoded input=\n{tokenizer.decode(input_ids, skip_special_tokens=True)}")
+
         # Run generation
         out = generate_vanilla(
             model=model,
